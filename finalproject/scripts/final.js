12bb98bcba60cb83f4f94b4daed5aff0fa58e205
Gamujob <jgamutambuli@byupathway.edu>
2026-04-18 18:09:17 +0300

    added local storage

--- finalproject/scripts/final.js
+++ finalproject/scripts/final.js
@@ -19,3 +19,35 @@ hamBurger.addEventListener('click', () => {
 // FOR TIMES STAMP
 const timestamp = document.querySelector('#loadtimestamp');
 timestamp.value = new Date().toISOString();
+
+// Local storage, and visit messages
+const visitMessage = document.querySelector('#localStore')
+const lastVisit = localStorage.getItem('lastVisit')
+const now = Date.now()
+
+let message = "";
+
+if (!lastVisit) {
+  // this is for the first visit
+  message = `Welcome! Let us know if you have any question.`
+} else {
+  const lastVt = Number(lastVisit)
+  const diffTime = now - lastVt
+
+  const oneDay = 1000 * 60 * 60 * 24
+  const diffDays = Math.floor(diffTime / oneDay)
+
+  if (diffDays < 1) {
+    message = `Back so soon! Awesome!`
+  } else if (diffDays === 1) {
+    message = `You last visited 1 day ago.`
+  } else {
+    message = `You last visited ${diffDays} days ago.`
+  }
+}
+
+// displaying
+visitMessage.textContent = message;
+
+// save the current visit
+localStorage.setItem('lastVisit', now);
